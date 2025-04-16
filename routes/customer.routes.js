7a4b8455e31334160a72e6bbffe164a969d56b4f
Vatsal Mavani <vatsal9999.vm@gmail.com>
2025-04-16 12:53:01 +0530

    completed demo

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const customerController = require("../controllers/customer.controller");
+const { authorizeRoles } = require("../middleware/auth");
 
 router.post("/", customerController.createCustomer);
 router.get("/", customerController.getCustomers);
 router.get("/:id", customerController.getCustomer);
-router.put("/:id", customerController.updateCustomer);
-router.delete("/:id", customerController.deleteCustomer);
+router.put("/:id", authorizeRoles("admin"), customerController.updateCustomer);
+router.delete(
+  "/:id",
+  authorizeRoles("admin"),
+  customerController.deleteCustomer
+);
 
 module.exports = router;
--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const salesController = require("../controllers/sales.controller");
+const { authorizeRoles } = require("../middleware/auth");
 
 router.post("/", salesController.createSales);
 router.get("/", salesController.getSales);
 router.put("/:id/status", salesController.updateSalesStatus);
 router.get("/:id/invoice", salesController.getInvoice);
 router.put("/:id", salesController.updateSales);
-router.delete("/:id", salesController.deleteSales);
+router.delete("/:id", authorizeRoles("admin"), salesController.deleteSales);
 
 module.exports = router;
